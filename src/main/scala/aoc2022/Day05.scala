e52c7058781d35abffe991e24f4e678ef8b67d5e
Cosmin Stroe <oncall@cosmin.cloud>
2022-12-04 23:41:47 -0600

    day 05

---
+++ src/main/scala/aoc2022/Day05.scala
@@ -0,0 +1,110 @@
+package aoc2022
+
+object Day05 {
+  def main(args: Array[String]): Unit = {
+    val contents = readFileToLines("src/main/resources/day05/input")
+    /*
+    [T]             [P]     [J]
+    [F]     [S]     [T]     [R]     [B]
+    [V]     [M] [H] [S]     [F]     [R]
+    [Z]     [P] [Q] [B]     [S] [W] [P]
+    [C]     [Q] [R] [D] [Z] [N] [H] [Q]
+    [W] [B] [T] [F] [L] [T] [M] [F] [T]
+    [S] [R] [Z] [V] [G] [R] [Q] [N] [Z]
+    [Q] [Q] [B] [D] [J] [W] [H] [R] [J]
+     1   2   3   4   5   6   7   8   9
+     */
+    val stackRepresentation = contents.takeWhile(_.nonEmpty).toList
+    //  1   2   3   4   5   6   7   8   9
+    val numStacks = stackRepresentation.last.count(_ != 32)
+
+    val stacks = Range(0, numStacks).map { i =>
+      scala.collection.mutable.Stack[String]()
+    }
+
+    Range.inclusive(stackRepresentation.length-2, 0, -1).map { i =>
+      stackRepresentation(i)
+    }.foreach { line =>
+      Range(0, numStacks).foreach { stackNum =>
+        val crate = line.substring((stackNum*4)+1,(stackNum*4)+2)
+        if (crate != " ") {
+          stacks(stackNum).push(crate)
+        }
+      }
+    }
+
+    val instrRegex = "move ([0-9]+) from ([0-9]+) to ([0-9]+)".r
+    contents.drop(stackRepresentation.length+1).foreach { instr =>
+      for (patternMatch <- instrRegex.findAllMatchIn(instr)) {
+        val moveThisMany = patternMatch.group(1).toInt
+        val fromStack = patternMatch.group(2).toInt - 1
+        val toStack = patternMatch.group(3).toInt - 1
+        Range(0, moveThisMany).foreach { _ =>
+          val crate = stacks(fromStack).pop()
+          stacks(toStack).push(crate)
+        }
+      }
+    }
+
+    val output = stacks.map(_.top).mkString("")
+    println(s"Output is: $output")
+  }
+}
+
+object Day05Part2 {
+  def main(args: Array[String]): Unit = {
+    val contents = readFileToLines("src/main/resources/day05/input")
+    /*
+    [T]             [P]     [J]
+    [F]     [S]     [T]     [R]     [B]
+    [V]     [M] [H] [S]     [F]     [R]
+    [Z]     [P] [Q] [B]     [S] [W] [P]
+    [C]     [Q] [R] [D] [Z] [N] [H] [Q]
+    [W] [B] [T] [F] [L] [T] [M] [F] [T]
+    [S] [R] [Z] [V] [G] [R] [Q] [N] [Z]
+    [Q] [Q] [B] [D] [J] [W] [H] [R] [J]
+     1   2   3   4   5   6   7   8   9
+     */
+    val stackRepresentation = contents.takeWhile(_.nonEmpty).toList
+    //  1   2   3   4   5   6   7   8   9
+    val numStacks = stackRepresentation.last.count(_ != 32)
+
+    val stacks = Range(0, numStacks).map { i =>
+      scala.collection.mutable.Stack[String]()
+    }
+
+    Range.inclusive(stackRepresentation.length - 2, 0, -1).map { i =>
+      stackRepresentation(i)
+    }.foreach { line =>
+      Range(0, numStacks).foreach { stackNum =>
+        val crate = line.substring((stackNum * 4) + 1, (stackNum * 4) + 2)
+        if (crate != " ") {
+          stacks(stackNum).push(crate)
+        }
+      }
+    }
+
+    val instrRegex = "move ([0-9]+) from ([0-9]+) to ([0-9]+)".r
+    contents.drop(stackRepresentation.length + 1).foreach { instr =>
+      for (patternMatch <- instrRegex.findAllMatchIn(instr)) {
+        val moveThisMany = patternMatch.group(1).toInt
+        val fromStack = patternMatch.group(2).toInt - 1
+        val toStack = patternMatch.group(3).toInt - 1
+        val tempStack = scala.collection.mutable.Stack[String]()
+        Range(0, moveThisMany).foreach { _ =>
+          val crate = stacks(fromStack).pop()
+          tempStack.push(crate)
+        }
+        Range(0, moveThisMany).foreach { _ =>
+          val crate = tempStack.pop()
+          stacks(toStack).push(crate)
+        }
+      }
+    }
+
+    val output = stacks.map(_.top).mkString("")
+    println(s"Output is: $output")
+  }
+
+}
+
