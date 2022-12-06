fa6b4d8d7ff5fa1b933599f5b0c2f1e1dada3e86
Cosmin Stroe <oncall@cosmin.cloud>
2022-12-05 23:14:21 -0600

    day 06

---
+++ src/main/scala/aoc2022/Day06.scala
@@ -0,0 +1,32 @@
+package aoc2022
+
+object Day06 {
+  def main(args: Array[String]): Unit = {
+    val contents = readFile("src/main/resources/day06/input")
+    // _ ( _ _ _ _)
+    val packet = contents
+      .sliding(4)
+      .zipWithIndex
+      .find { w =>
+        w._1.toCharArray.toSet.size == 4
+      }
+      .get
+
+    print(packet._2 + 4)
+  }
+}
+
+object Day06Part2 {
+  def main(args: Array[String]): Unit = {
+    val contents = readFile("src/main/resources/day06/input")
+    val packet = contents
+      .sliding(14)
+      .zipWithIndex
+      .find { w =>
+        w._1.toCharArray.toSet.size == 14
+      }
+      .get
+
+    print(packet._2 + 14)
+  }
+}
