503b44a8e480f8d515b0ecf1141e144db8b63f60
Cosmin Stroe <oncall@cosmin.cloud>
2022-12-13 02:35:06 -0600

    more cleanup

--- src/main/scala/aoc2022/Day13Part2.scala
+++ src/main/scala/aoc2022/Day13Part2.scala
@@ -4,11 +4,10 @@ import aoc2022.Day13._
 
 object Day13Part2 {
   def main(args: Array[String]): Unit = {
+    val markerPackets = List("[[2]]", "[[6]]")
     val output: Int =
-      (readFileToLines("src/main/resources/day13/input") ++ List(
-        "[[2]]",
-        "[[6]]"
-      )).filterNot(_.trim.isBlank)
+      (readFileToLines("src/main/resources/day13/input") ++ markerPackets)
+        .filterNot(_.trim.isBlank)
         .sortInPlaceWith { case (left, right) =>
           compare(left, right) match {
             case Smaller | LeftOutOfItems => true
@@ -16,10 +15,8 @@ object Day13Part2 {
           }
         }
         .zipWithIndex
-        .map { case (term, index) => (term, index + 1) }
-        .filter { case (term, _) =>
-          term == "[[2]]" || term == "[[6]]"
-        }
+        .map { case (term, i) => (term, i + 1) }
+        .filter { case (term, _) => markerPackets.contains(term) }
         .map(_._2)
         .product
 
