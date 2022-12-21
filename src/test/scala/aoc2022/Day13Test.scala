fcc47438801bd6a2648f27ccab6b4667ecc9d99e
Cosmin Stroe <oncall@cosmin.cloud>
2022-12-21 16:45:15 -0600

    convert day 13 test to junit, but keep matchers

--- src/test/scala/aoc2022/Day13Test.scala
+++ src/test/scala/aoc2022/Day13Test.scala
@@ -1,12 +1,13 @@
 package aoc2022
 
 import aoc2022.Day13.{LeftOutOfItems, RightOutOfItems}
+import org.junit.Test
 import org.scalatest.matchers.should.Matchers
 
-class Day13Test extends org.scalatest.flatspec.AnyFlatSpec with Matchers {
-  behavior of "Comparison"
+class Day13Test extends Matchers {
 
-  it should "correctly do the example inputs" in {
+  @Test
+  def correctlyDoTheExampleInputs(): Unit = {
     Day13.inOrder("[1,1,3,1,1]", "[1,1,5,1,1]") shouldBe true
     Day13.inOrder("[[1],[2,3,4]]", "[[1],4]") shouldBe true
     Day13.inOrder("[9]", "[[8,7,6]]") shouldBe false
@@ -23,13 +24,15 @@ class Day13Test extends org.scalatest.flatspec.AnyFlatSpec with Matchers {
     Day13.compare("[]", "[3]") shouldBe LeftOutOfItems
   }
 
-  it should "compare equal lists of different sizes" in {
+  @Test
+  def itShouldCompareEqualListsOfDifferentSizes(): Unit = {
     Day13.inOrder("[7,7,7,7]", "[7,7,7]") shouldBe false
     Day13.inOrder("[7,7,7]", "[7,7,7]") shouldBe true
     Day13.inOrder("[7,7,7]", "[7,7,7,7]") shouldBe true
   }
 
-  it should "compare with lists" in {
+  @Test
+  def itShouldCompareWithLists(): Unit = {
     Day13.inOrder("[2,3,4]", "[4]") shouldBe true
   }
 }
