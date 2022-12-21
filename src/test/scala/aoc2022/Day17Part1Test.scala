7c47d152de77284628e09c43b0ae00ab846f485a
Cosmin Stroe <oncall@cosmin.cloud>
2022-12-21 16:47:52 -0600

    convert to junit

--- src/test/scala/aoc2022/Day17Part1Test.scala
+++ src/test/scala/aoc2022/Day17Part1Test.scala
@@ -1,12 +1,13 @@
 package aoc2022
 
 import aoc2022.Day17Part1.{CrossRock, FallingRock, FlatRock, Point}
+import org.junit.Test
 import org.scalatest.matchers.should.Matchers
 
-class Day17Part1Test extends org.scalatest.flatspec.AnyFlatSpec with Matchers {
-  behavior of "FallingRock"
+class Day17Part1Test extends Matchers {
 
-  it should "correctly create points" in {
+  @Test
+  def correctlyCreatePoints(): Unit = {
     FallingRock(FlatRock(), Point(0, 0)).toPoints() shouldBe Set(
       Point(0,0), Point(1,0), Point(2,0), Point(3,0)
     )
@@ -15,7 +16,8 @@ class Day17Part1Test extends org.scalatest.flatspec.AnyFlatSpec with Matchers {
     )
   }
 
-  it should "correctly find intersecting rocks" in {
+  @Test
+  def correctlyFindIntersectingRocks(): Unit = {
     val bottomRock = FallingRock(FlatRock(), Point(0, 0))
     val topRock = FallingRock(FlatRock(), Point(0, 1))
   }
